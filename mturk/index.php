4e0e099aee3e84cc5decf68ede729ce65a119bc5
Katie Kuksenok <katie.kuksenok@gmail.com>
2013-03-04 15:50:55 -0800

    stuff

---
+++ mturk/index.php
@@ -0,0 +1,16 @@
+<?php
+include("utils/Queries.php");
+include("utils/Response.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $r = new Response();
+        ?>
+    </body>
+</html>
