775637b2d33172d846e7930017008aacff189866
Katie Kuksenok <katie.kuksenok@gmail.com>
2013-03-19 17:29:30 -0700

    Added API

---
+++ diamond/data.php
@@ -0,0 +1,17 @@
+<?php
+
+$results = array(
+	"anger" => array(
+		"TTF" => 50,
+		"TFF" => 50,
+		"TTT" => 50,
+		"FFF" => 50
+		),
+	"interest" => array(
+		"TTT" => 40000
+		)
+	);
+
+echo json_encode($results);
+
+?>
---
+++ diamond/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A Title </title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="color.js"></script>
+	<script>
+		window.special_triangle_classes = [];
+		window.special_triangle_classes["FTT"] = "triangle-topleft";
+		$.getJSON('data.php', function(data) {
+			$.each(data.codes, function(code, diamond_data){
+            }
+		});
+	</script>
+	<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+	<div>
+		<h1>Heading1</h1>
+		<p>paragraph.</p>
+		
+		<h1>Heading2</h1>
+		<p>paragraph.</p>
+		
+		<h1>Heading3</h1>
+		<p>paragraph.</p>
+	</div>
+	<div id="legend">
+		<img src="legend.jpg" alt="legend" width="60%" height="60%">
+	</div>
+	<div id="toprow">
+		<div id="FTT" class="triangle-topleft">15</div>
+		<div id="FTTtext"><p>15</p></div>
+		<div class="squaresbackground" id="FTTbackground"></div>
+		<div class="FTTsquares">
+			<div id="historic" class="squareblack"></div>
+			<div id="current" class="squarewhite"></div>
+			<div id="auto" class="squarewhite"></div>
+		</div>
+
+		<div id="TTT" class="triangle-bottomright">10</div>
+		<div id="TTTtext"><p>10</p></div>
+		<div class="squaresbackground" id="TTTbackground"></div>
+		<div class="TTTsquares">
+			<div id="historic" class="squarewhite"></div>
+			<div id="current" class="squarewhite"></div>
+			<div id="auto" class="squarewhite"></div>
+		</div>
+		
+		<div id="TFT" class="triangle-bottomleft">13</div>
+		<div id="TFTtext"><p>13</p></div>
+		<div class="squaresbackground" id="TFTbackground"></div>
+		<div class="TFTsquares">
+			<div id="historic" class="squarewhite"></div>
+			<div id="current" class="squareblack"></div>
+			<div id="auto" class="squarewhite"></div>
+		</div>
+		
+		<div id="TTF" class="triangle-topright">26</div>
+		<div id="TTFtext"><p>26</p></div>
+		<div class="squaresbackground" id="TTFbackground"></div>
+		<div class="TTFsquares">
+			<div id="historic" class="squarewhite"></div>
+			<div id="current" class="squarewhite"></div>
+			<div id="auto" class="squareblack"></div>
+		</div>
+		
+	</div>
+	<div id="bottomrow">
+		<div id="FFT" class="triangle-bottomleft">3</div>
+		<div id="FFTtext"><p>3</p></div>
+		<div class="squaresbackground" id="FFTbackground"></div>
+		<div class="FFTsquares">		
+			<div id="historic" class="squareblack"></div>
+			<div id="current" class="squareblack"></div>
+			<div id="auto" class="squarewhite"></div>
+		</div>
+				
+		<div id="FTF" class="triangle-topright">5</div>
+		<div id="FTFtext"><p>5</p></div>
+		<div class="squaresbackground" id="FTFbackground"></div>
+		<div class="FTFsquares">		
+			<div id="historic" class="squareblack"></div>
+			<div id="current" class="squarewhite"></div>
+			<div id="auto" class="squareblack"></div>
+		</div>
+		
+		<div id="FFF" class="triangle-topleft">27</div>
+		<div id="FFFtext"><p>27</p></div>
+		<div class="squaresbackground" id="FFFbackground"></div>
+		<div class="FFFsquares">		
+			<div id="historic" class="squareblack"></div>
+			<div id="current" class="squareblack"></div>
+			<div id="auto" class="squareblack"></div>
+		</div>
+		
+		<div id="TFF" class="triangle-bottomright">1</div>
+		<div id="TFFtext"><p>1</p></div>
+		<div class="squaresbackground" id="TFFbackground"></div>
+		<div class="TFFsquares">		
+			<div id="historic" class="squarewhite"></div>
+			<div id="current" class="squareblack"></div>
+			<div id="auto" class="squareblack"></div>
+		</div>
+		
+	</div>
+
+</body>
+</html>
