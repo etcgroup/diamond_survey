8007ed1a401277b422ec5a89ee1637fd9a34f322
Megan Torkildson <mtorkild@uw.edu>
2013-03-23 10:29:27 -0700

    Fixed Initial tooltips
    Only half appear

--- diamond/index.php
+++ diamond/index.php
@@ -2,12 +2,24 @@
 <html>
 <head>
 	<title>A Title </title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
         <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	<script src="color.js"></script>
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
 	<link rel="stylesheet" href="styles.css" />
+        <script>
+            $(function() {
+              $( document ).tooltip();
+            });
+            //$("#test label[title]").tooltip();
+        </script>
+        <style>
+            label {
+              display: inline-block;
+              width: 5em;
+            }
+        </style>
 </head>
 <body>
 	<div id="canvas"></div>
