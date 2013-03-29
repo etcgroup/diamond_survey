94ddca0b62a67768bac8327d59793c42f631fe2a
Katie Kuksenok <katie.kuksenok@gmail.com>
2013-03-29 10:07:09 -0700

    Survey 2 interactions working

--- survey/survey2.php
+++ survey/survey2.php
@@ -11,6 +11,31 @@ include("utils/UI.php");
 	<script src="survey2.js"></script>
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
 	<link rel="stylesheet" href="survey2.css" />
+   <script>
+	var time_start = new Date();
+	var time_example = [];
+	var time_help = [];
+	var timer = 0;
+	
+	// Adding up modal sessions and submitting the values
+	function submitTime() {
+		curr_time = new Date();
+		var total_time = document.getElementById("total-survey-time");
+		var survey_time = Math.round((curr_time - window.time_start) / 1000);
+		total_time.value = survey_time;
+	}
+	$(function() {
+		$('#key-button').on("click",function(){
+			if($(this).text()=="[-]"){
+				$(this).text("[+]");
+				$("#key").css("right", "-250px");
+			} else{
+				$(this).text("[-]");
+				$("#key").css("right", "-30px");
+			}
+		});
+	});
+</script>
 </head>
 <body>
     <div class="box">
@@ -48,30 +73,11 @@ include("utils/UI.php");
     </div>
     <div id="canvases">
 	 <form method="post" action="submit.php">
-           <div id="canvas1" class="box deselect_text"></div>
-           <div id="canvas2" class="box deselect_text"></div>
-           <div id="canvas3" class="taskbox deselect_text"></div>
-           <div id="canvas4" class="taskbox deselect_text"></div>
-           
+           <div id="canvas1" class="canvas box deselect_text"></div>
+           <div id="canvas2" class="canvas box deselect_text"></div>
+           <div id="canvas3" class="canvas box deselect_text"></div>
+           <div id="canvas4" class="canvas box deselect_text"></div>
     </div>
-    <div id="key">
-        <p>
-            <button id="key-button">[-]</button>
-            <em>Legend:</em>
-            <p class="img"><img src="legend.PNG" alt="" height="150"></p>
-            <em>Example Interpretation:</em>
-            <p class="img">
-                <img src="annotatedexample.PNG" alt="" height="100">
-            <ul class="example_interp">
-                <li>20 messages were labeled true by all three data sets (Manual, Inferred, Verifying).</li>
-                <li>26 messages were labeled as false by Manual, but true by Inferred and Verifying. This indicates an interesting change between the Manual and Verifying data sets.</li>
-                <li>34 messages were labeled true in Manual, but false by Inferred and Verifying. This also indicates a change between the Manual and Verifying data.</li>
-                <li>The 26 and 34 percent message groups signify that there was a severe code definition shift problem.</li>
-            </ul>
-            </p>
-        </p>
-    </div>
-	   
 	   <div class="box">
 
 			<p><span class="question">Question 7.</span> On a scale from 1 (Disagree) to 5 (Agree)
@@ -109,11 +115,32 @@ include("utils/UI.php");
 		</div>
 	   
 		<div class="box">
-		   <input name="answer" id="answer" type="hidden" value="" />
-                    <input name="type" id="type" type="hidden" value="" />
-                    <input name="hill" id="hill" type="hidden" value="" />
+			<input name="answer" id="answer" type="hidden" value="" />
+			<input name="type" id="type" type="hidden" value="" />
+			<input name="hill" id="hill" type="hidden" value="" />
+			<input type="hidden" name="total-survey-time" id="total-survey-time" value="" />
+			<input type="submit" onclick="submitTime()" class="submit" />
 		</div>
 	</form>
+	
 </div>
+    <div id="key">
+        <p>
+            <button id="key-button">[-]</button>
+            <em>Legend:</em>
+            <p class="img"><img src="img/legend.PNG" alt="" height="150"></p>
+            <em>Example Interpretation:</em>
+            <p class="img">
+                <img src="img/annotatedexample.PNG" alt="" height="100">
+            <ul class="example_interp">
+                <li>20 messages were labeled true by all three data sets (Manual, Inferred, Verifying).</li>
+                <li>26 messages were labeled as false by Manual, but true by Inferred and Verifying. This indicates an interesting change between the Manual and Verifying data sets.</li>
+                <li>34 messages were labeled true in Manual, but false by Inferred and Verifying. This also indicates a change between the Manual and Verifying data.</li>
+                <li>The 26 and 34 percent message groups signify that there was a severe code definition shift problem.</li>
+            </ul>
+            </p>
+        </p>
+    </div>
+	   
 </body>
 </html>
